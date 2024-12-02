3c6b20779f297238ca8314b2628e3e032f115aec
hsiangcoco <f1346790@gmail.com>
2024-12-02 13:01:22 +0800

    1102 測試

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react"
 import axios from "axios"
 import './App.css'
+import { FaUmbrella } from "react-icons/fa";
+
 
 function App() {
 
   useEffect(() => {
     (async () => {
-      const data = await axios.get('./F-C0032-001.json');
+      const data = await axios.get('https://raw.githubusercontent.com/Hsiangcoco/myreact/refs/heads/main/my-react-app1/src/F-C0032-001.json');
       const { location } = data.data.cwaopendata.dataset;
       console.log(location)
 
@@ -16,81 +18,86 @@ function App() {
 
   return (
     <>
-      <h2>36小時 天氣預報</h2>
-      <h2>三十六小時天氣預報</h2>
-      <div id="Card">
-        <div id="card1">
-          <div id="city">
-            <h3>台北市</h3>
-          </div>
-          <div id="dataWeather">
-            <div id="dataWeather1">
-              <p>2日</p>
-              <p>上午12:00</p>
-              <p>~</p>
-              <p>下午12:00</p>
-              <p><img src="./weatherIcon/多雲.svg" alt="" /></p>
-              <p>晴時多雲</p>
-              <p>10%</p>
+      <div id="Weather">
+        <h2>36小時 天氣預報</h2>
+        <h2>三十六小時天氣預報</h2>
+        <div id="Card">
+          <div className="card">
+            <div className="city">
+              <h3>台北市</h3>
             </div>
-            <div id="dataWeather2">
-              <p>2日</p>
-              <p>上午12:00</p>
-              <p>~</p>
-              <p>下午12:00</p>
-              <p><img src="./weatherIcon/多雲.svg" alt="" /></p>
-              <p>晴時多雲</p>
-              <p>10%</p>
-            </div>
-            <div id="dataWeather3">
-              <p>2日</p>
-              <p>上午12:00</p>
-              <p>~</p>
-              <p>下午12:00</p>
-              <p><img src="./weatherIcon/多雲.svg" alt="" /></p>
-              <p>晴時多雲</p>
-              <p>10%</p>
+
+            <div className="dataWeather">
+              <div className="row">
+                <p>2日</p>
+                <p>上午12:00</p>
+                <p>~</p>
+                <p>下午12:00</p>
+                <p><img src="./weatherIcon/多雲.svg" alt="" /></p>
+                <p>晴時多雲</p>
+                <p><FaUmbrella />10%</p>
+              </div>
+              <div className="row">
+                <p>2日</p>
+                <p>上午12:00</p>
+                <p>~</p>
+                <p>下午12:00</p>
+                <p><img src="./weatherIcon/多雲.svg" alt="" /></p>
+                <p>晴時多雲</p>
+                <p><FaUmbrella />10%</p>
+              </div>
+              <div className="row">
+                <p>2日</p>
+                <p>上午12:00</p>
+                <p>~</p>
+                <p>下午12:00</p>
+                <p><img src="./weatherIcon/多雲.svg" alt="" /></p>
+                <p>晴時多雲</p>
+                <p><FaUmbrella />
+                10%</p>
+              </div>
+
             </div>
 
           </div>
+          <div className="card">
+            <div className="city">
+              <h3>台北市</h3>
+            </div>
+            <div className="dataWeather">
+              <div className="row">
+                <p>2日</p>
+                <p>上午12:00</p>
+                <p>~</p>
+                <p>下午12:00</p>
+                <p><img src="./weatherIcon/多雲.svg" alt="" /></p>
+                <p>晴時多雲</p>
+                <p><FaUmbrella />10%</p>
+              </div>
+              <div className="row">
+                <p>2日</p>
+                <p>上午12:00</p>
+                <p>~</p>
+                <p>下午12:00</p>
+                <p><img src="./weatherIcon/多雲.svg" alt="" /></p>
+                <p>晴時多雲</p>
+                <p><FaUmbrella />10%</p>
+              </div>
+              <div className="row">
+                <p>2日</p>
+                <p>上午12:00</p>
+                <p>~</p>
+                <p>下午12:00</p>
+                <p><img src="./weatherIcon/多雲.svg" alt="" /></p>
+                <p>晴時多雲</p>
+                <p><FaUmbrella />10%</p>
+              </div>
 
+            </div>
+
+          </div>
         </div>
-        <div id="card2">
-          <div id="city">
-            <h3>台北市</h3>
-          </div>
-          <div id="dataWeather">
-            <div id="dataWeather1">
-              <p>2日</p>
-              <p>上午12:00</p>
-              <p>~</p>
-              <p>下午12:00</p>
-              <p><img src="./weatherIcon/多雲.svg" alt="" /></p>
-              <p>晴時多雲</p>
-              <p>10%</p>
-            </div>
-            <div id="dataWeather2">
-              <p>2日</p>
-              <p>上午12:00</p>
-              <p>~</p>
-              <p>下午12:00</p>
-              <p><img src="./weatherIcon/多雲.svg" alt="" /></p>
-              <p>晴時多雲</p>
-              <p>10%</p>
-            </div>
-            <div id="dataWeather3">
-              <p>2日</p>
-              <p>上午12:00</p>
-              <p>~</p>
-              <p>下午12:00</p>
-              <p><img src="./weatherIcon/多雲.svg" alt="" /></p>
-              <p>晴時多雲</p>
-              <p>10%</p>
-            </div>
 
-          </div>
-
-        </div>
       </div>
     </>
   )
